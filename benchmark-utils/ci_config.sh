c49afb3506cca396f0969a8adb22b75a83e73d6c
Bajczi Levente <bajczilevi@gmail.com>
2026-04-27 12:42:05 +0200

    Update timeout command in ci_config.sh

--- benchmark-utils/ci_config.sh
+++ benchmark-utils/ci_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export BENCHMARK="timeout 360s benchexec/bin/benchexec"
+export BENCHMARK="timeout -k 60 360 benchexec/bin/benchexec"
 export BENCHMARK_PARAMS="--read-only-dir=/ \
                          --overlay-dir=/home \
                          --limitCores '-1' \
